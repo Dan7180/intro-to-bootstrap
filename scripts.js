83868c5ca90500b1ec0f6abb507a74e5b9e83251
Daniel O. Oketch <danielochieng967@gmail.com>
2020-10-18 22:22:09 +0300

    final

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+$( "#loginModalButton" ).on( "click", function() {
+  console.log( "hello");
+  $('#loginModal').modal('show')
+});
+
+
+
+$( "#mymodalbutton" ).on( "click", function() {
+    console.log( "hello");
+    $('#ReserveModal').modal('show')
+  });
